a4cc7d8eb27bb09502ae7ee921c9ed7095720013
Pascal Hartig <passy@twitter.com>
2014-09-26 09:49:33 +0100

    Upgrade to new Yeoman Generator extend mechanism

--- app/index.js
+++ app/index.js
@@ -10,99 +10,99 @@ function escapeRegExp(str) {
   return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
 }
 
-var HerokuGenerator = module.exports = function HerokuGenerator(args, options) {
-  yeoman.generators.Base.apply(this, arguments);
+module.exports = yeoman.generators.Base.extend({
 
-  this.options = options;
-  this.distDir = options.dist || 'dist';
-};
+  constructor: function () {
+    yeoman.generators.Base.apply(this, arguments);
 
-util.inherits(HerokuGenerator, yeoman.generators.Base);
+    this.distDir = this.options.dist || 'dist';
+  },
 
-HerokuGenerator.prototype.checkInstallation = function checkInstallation() {
-  var done = this.async();
+  checkInstallation: function checkInstallation() {
+    var done = this.async();
 
-  this.herokuInstalled = false;
-  exec('heroku --version', function (err) {
-    if (err) {
-      this.log.error('You don\'t have the Heroku Toolbelt installed. ' +
-                     'Grab it from https://toolbelt.heroku.com/');
-    } else {
-      this.herokuInstalled = true;
-    }
-    done();
-  }.bind(this));
-};
-
-HerokuGenerator.prototype.askFor = function askFor() {
-  var cb = this.async();
-
-  var prompts = [{
-    name: 'distRepo',
-    message: 'Do you want a separate git repository in dist/?',
-    type: 'confirm'
-  }];
-
-  this.prompt(prompts, function (props) {
-    this.distRepo = props.distRepo;
-
-    cb();
-  }.bind(this));
-};
-
-HerokuGenerator.prototype.nodestatic = function nodestatic() {
-  this.mkdir('heroku');
-  this.copy('Procfile', 'heroku/Procfile');
-  this.copy('server.js', 'heroku/server.js');
-};
-
-HerokuGenerator.prototype.distpackage = function distpackage() {
-  var pkgPath = path.resolve('package.json');
-  var pkg = {};
-
-  if (fs.existsSync(pkgPath)) {
-    pkg = JSON.parse(this.readFileAsString('package.json'));
-  }
-
-  var distPkg = {
-    name: pkg.name || 'unnamed',
-    version: '0.0.0',
-    dependencies: {
-      'statik': '~1.2.5'
-    }
-  };
-
-  this.write('heroku/distpackage.json', JSON.stringify(distPkg, null, 2));
-};
-
-HerokuGenerator.prototype.rewiregrunt = function rewiregrunt() {
-  var template = this.readFileAsString(path.join(__dirname, 'templates', 'copytemplate.js'));
-
-  console.log(
-    chalk.yellow('Please add this copy task rule to your Gruntfile: \n') +
-    template
-  );
-};
-
-HerokuGenerator.prototype.gitsetup = function gitsetup() {
-  if (this.distRepo) {
-    exec('git init && git add -A && git commit -m "Initial commit"', { cwd: this.distDir });
-    console.log(chalk.green('You\'re all set! Now go to ' + chalk.bold(this.distDir) + ' and run\n\t') +
-                chalk.bold('heroku apps:create'));
-  } else {
-    fs.readFile('.gitignore', { encoding: 'utf-8' }, function (err, data) {
+    this.herokuInstalled = false;
+    exec('heroku --version', function (err) {
       if (err) {
-        return;
+        this.log.error('You don\'t have the Heroku Toolbelt installed. ' +
+                      'Grab it from https://toolbelt.heroku.com/');
+      } else {
+        this.herokuInstalled = true;
       }
-
-      // Remove dist/ ignore
-      data = data.replace(new RegExp(escapeRegExp(this.distDir) + '\/?\n', 'g'), '');
-
-      // Fire and forget
-      fs.writeFile('.gitignore', data);
+      done();
     }.bind(this));
-    console.log(chalk.green('You\'re all set! Now run\n\t' + chalk.bold('heroku apps:create') +
-                '\nand push your ' + this.distDir + ' directory with\n\t') +
-                chalk.bold('git subtree push --prefix dist heroku master'));
+  },
+
+  askFor: function askFor() {
+    var cb = this.async();
+
+    var prompts = [{
+      name: 'distRepo',
+      message: 'Do you want a separate git repository in dist/?',
+      type: 'confirm'
+    }];
+
+    this.prompt(prompts, function (props) {
+      this.distRepo = props.distRepo;
+
+      cb();
+    }.bind(this));
+  },
+
+  nodestatic: function nodestatic() {
+    this.mkdir('heroku');
+    this.copy('Procfile', 'heroku/Procfile');
+    this.copy('server.js', 'heroku/server.js');
+  },
+
+  distpackage: function distpackage() {
+    var pkgPath = path.resolve('package.json');
+    var pkg = {};
+
+    if (fs.existsSync(pkgPath)) {
+      pkg = JSON.parse(this.readFileAsString('package.json'));
+    }
+
+    var distPkg = {
+      name: pkg.name || 'unnamed',
+      version: '0.0.0',
+      dependencies: {
+        'statik': '~1.2.5'
+      }
+    };
+
+    this.write('heroku/distpackage.json', JSON.stringify(distPkg, null, 2));
+  },
+
+  rewiregrunt: function rewiregrunt() {
+    var template = this.readFileAsString(path.join(__dirname, 'templates', 'copytemplate.js'));
+
+    console.log(
+      chalk.yellow('Please add this copy task rule to your Gruntfile: \n') +
+      template
+    );
+  },
+
+  gitsetup: function gitsetup() {
+    if (this.distRepo) {
+      exec('git init && git add -A && git commit -m "Initial commit"', { cwd: this.distDir });
+      console.log(chalk.green('You\'re all set! Now go to ' + chalk.bold(this.distDir) + ' and run\n\t') +
+                  chalk.bold('heroku apps:create'));
+    } else {
+      fs.readFile('.gitignore', { encoding: 'utf-8' }, function (err, data) {
+        if (err) {
+          return;
+        }
+
+        // Remove dist/ ignore
+        data = data.replace(new RegExp(escapeRegExp(this.distDir) + '\/?\n', 'g'), '');
+
+        // Fire and forget
+        fs.writeFile('.gitignore', data);
+      }.bind(this));
+      console.log(chalk.green('You\'re all set! Now run\n\t' + chalk.bold('heroku apps:create') +
+                  '\nand push your ' + this.distDir + ' directory with\n\t') +
+                  chalk.bold('git subtree push --prefix dist heroku master'));
+    }
   }
-};
+});
